5cbf42b92dc7628eb515be875d4eb51e2eb11ce3
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2022-11-27 00:05:24 +0600

    product delete api created

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
             res.send(products)
         })
 
+        // delete a product
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // post user
         app.post('/users', async (req, res) => {
             const user = req.body
